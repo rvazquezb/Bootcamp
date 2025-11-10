7addca68d586039cd1e9cef1a1f72773b92a1ecd
rvazquezb <raul_vazquez97@hotmail.com>
2025-11-10 10:39:19 +0100

    arima por sarima

--- App/app.py
+++ App/app.py
@@ -324,7 +324,12 @@ def graficos(df):
         with all_tabs[3]:
             forecast_days = st.slider("Días a predecir", 7, 90, 30)
             df_features, forecast_results_rf, future_dates, historical_mae_rf, backtest_results, backtest_mae_rf = run_sklearn_prediction(df, forecast_periods=forecast_days)
-            forecast_results_sarima, mae_sarima, model_fit_sarima = run_sarima_prediction(df)
+            forecast_results_sarima, mae_sarima, model_fit_sarima = run_sarima_prediction(
+                df, 
+                forecast_periods=forecast_days, 
+                order=(1, 1, 1), 
+                seasonal_order=(0, 0, 0, 0) # <--- ¡EL CAMBIO CLAVE!
+            )
             sarima_succeeded = forecast_results_sarima is not None and mae_sarima is not None
             st.header("Análisis de Rendimiento del Modelo")
 
